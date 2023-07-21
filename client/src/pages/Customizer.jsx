f3e1a08020d67bbc900e99a9083f709f63259010
Harsha <c.harsha0909@gmail.com>
2023-07-21 08:02:21 +0530

    Implemented download functionality

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useSnapshot } from "valtio";
 
 import config from "../config/config";
 import state from "../store";
-import { reader } from "../config/helpers";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
 import {
     AIPicker,
-    FilePicker,
-    CustomButton,
     ColorPicker,
+    CustomButton,
+    FilePicker,
     Tab,
 } from "../components";
 
@@ -184,6 +185,16 @@ const Customizer = () => {
                                 }}
                             />
                         ))}
+                        <button
+                            className="download-btn"
+                            onClick={downloadCanvasToImage}
+                        >
+                            <img
+                                src={download}
+                                alt="download_image"
+                                className="w-3/5 h-3/5 object-contain"
+                            />
+                        </button>
                     </motion.div>
                 </>
             )}
